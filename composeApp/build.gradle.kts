7038235e5f739512f8d0c344e33218a83a761abf
Luis Ricardo <luiscastro.parceiro@dnitservices.com>
2024-01-23 21:41:54 -0300

    estrutura inicial projeto
    estrutura inicial projeto

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,103 @@
+import org.jetbrains.compose.ExperimentalComposeLibrary
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.kotlinSerialization)
+
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+
+        androidMain.dependencies {
+            implementation(libs.compose.ui.tooling.preview)
+            implementation(libs.androidx.activity.compose)
+            implementation(libs.ktor.client.android)
+            //  implementation(libs.kotlinx.coroutines.android)
+        }
+        commonMain.dependencies {
+            implementation(libs.bundles.moko.resources)
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.components.resources)
+            implementation(libs.bundles.ktor)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.kotlin.serialization)
+            //  implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.media.kamel)
+            implementation(libs.koin.compose)
+            implementation(libs.voyager.koin)
+            // For navigation
+            implementation(libs.voyager)
+            implementation( libs.kotlin.serialization.new)
+            implementation(libs.voyager.transitions)
+            implementation("io.ktor:ktor-client-logging:2.3.7")
+
+
+        }
+
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+        }
+    }
+}
+
+android {
+    namespace = "br.com.composedrinks"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].res.srcDirs("src/androidMain/res")
+    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
+
+    defaultConfig {
+        applicationId = "br.com.composedrinks"
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    dependencies {
+        debugImplementation(libs.compose.ui.tooling)
+    }
+}
+
---
+++ composeApp/src/commonMain/kotlin/common/Constants.kt
@@ -0,0 +1,11 @@
+package common
+
+
+class Constants {
+    object BASEURL {
+        const val VALUE = "www.thecocktaildb.com/api/json/v1/"
+    }
+    object ROUTES {
+        const val SEARCH = "1/search.php?f=as"
+    }
+}
